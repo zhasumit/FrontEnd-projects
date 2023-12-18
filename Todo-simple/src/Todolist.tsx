609fd9bbc89b18dbff2995b1f78a413ad1a16454
zhasumit <sumitzha2057@gmail.com>
2023-12-18 12:00:33 +0530

    remove and all done btns

--- Todo-simple/src/Todolist.tsx
+++ Todo-simple/src/Todolist.tsx
@@ -29,32 +29,63 @@ function Todolist() {
 		setTodos([...todos, newTodo]);
 	};
 
+	const handleMarkAll = () => {
+		setTodos(
+			todos.map((todo) => {
+				return { ...todo, completed: true };
+			})
+		);
+	};
+
+	const handleRemoveAll = () => {
+		setTodos([]);
+	};
+
 	return (
-		<div className="todolist-main-container">
-			<h1>To do List </h1>
-			<ul>
-				{todos.map((todo) => (
-					<li
-						key={todo.id}
-						onClick={() => handleToggle(todo.id)}
-						style={{
-							textDecoration: todo.completed
-								? "line-through"
-								: "none",
-							color: todo.completed ? "#50c878" : "white",
-						}}
-					>
-						{todo.text}
-					</li>
-				))}
-			</ul>
-			<input
-				type="text"
-				placeholder="Add item"
-				onChange={(e) => setInput(e.currentTarget.value)}
-			/>
-			<button onClick={handleAdd}>➕</button>
-		</div>
+		<>
+			<div className="todolist-main-container">
+				<h1>To do List </h1>
+				<ul>
+					{todos.map((todo) => (
+						<li
+							key={todo.id}
+							onClick={() => handleToggle(todo.id)}
+							style={{
+								textDecoration: todo.completed
+									? "line-through"
+									: "none",
+								color: todo.completed ? "#50c878" : "white",
+							}}
+						>
+							{todo.text}
+						</li>
+					))}
+				</ul>
+				<input
+					id="inputfield"
+					type="text"
+					placeholder="Add item"
+					onChange={(e) => setInput(e.currentTarget.value)}
+				/>
+				<button
+					onClick={handleAdd}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") handleAdd;
+					}}
+				>
+					➕
+				</button>
+				<br />
+				<div className="btns">
+					<button id="blue" onClick={handleMarkAll}>
+						✔️ All done
+					</button>
+					<button id="pink" onClick={handleRemoveAll}>
+						❌ Remove all
+					</button>
+				</div>
+			</div>
+		</>
 	);
 }
 
